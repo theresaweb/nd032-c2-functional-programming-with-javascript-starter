878a526dbe8a307b382e6ca44e3f4d57d2ed6495
Theresa Newman <theresa.webdev@gmail.com>
2020-10-12 22:09:18 -0400

    more work on webpack

--- project/src/public/client.js
+++ project/src/public/client.js
@@ -69,9 +69,7 @@ const ImageOfTheDay = (apod) => {
     // If image does not already exist, or it is not from today -- request it again
     const today = new Date()
     const photodate = new Date(apod.date)
-    console.log(photodate.getDate(), today.getDate());
 
-    console.log(photodate.getDate() === today.getDate());
     if (!apod || apod.date === today.getDate() ) {
         getImageOfTheDay(store)
     }
--- project/webpack.config.js
+++ project/webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   entry: ["./src/public/client.js"],
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, './src/public/dist'),
     filename: 'js/bundle.js',
   }
 };
